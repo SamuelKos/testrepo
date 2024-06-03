6f3f50cf89b6da7663a6571724a0dd5f764f6e49
SamuelKos <89200126+SamuelKos@users.noreply.github.com>
2024-06-03 23:49:27 +0300

    Update event_test.py

--- event_test.py
+++ event_test.py
@@ -3,15 +3,15 @@ import tkinter
 
 class Editor(tkinter.Toplevel):
 
-	def __init__(self):
+	def __init__(self, root):
 		
-		self.root = tkinter.Tk()
+		self.root = root
 		super().__init__(self.root, class_='myclass', bd=4)
 
 		self.textwid = tkinter.Text(self.root)
 		self.textwid.pack()
 		self.textwid.bind('<Control-p>', self.mycallback2)
-		
+
 		
 		self.eventnum = 0
 		self.textwid.insert('1.0', 'asd')
@@ -20,21 +20,14 @@ class Editor(tkinter.Toplevel):
 		self.textwid.mark_set('insert', '1.0')
 		
 		self.after(300, self.textwid.event_generate('<Control-p>'))
-		self.do_nothing()
 		
 	
 	def mycallback2(self, event=None):
-		print(event.state, 'jou')
-		return 'break'
-		
-	def do_nothing(self):
-		x = 1
-		for i in range(10000):
-			x = x// 100
-			for n in range(10000):
-				x = x + n
-		return 'break'	
-		
+		print(event.state)
+		self.root.quit()
+		self.root.destroy()
+
+	
 	def mycallback(self, event=None):
 		self.eventnum += 1
 		
@@ -50,4 +43,13 @@ class Editor(tkinter.Toplevel):
 		print(10*'= ')
 		
 		return 'break'
+
+
+
+if __name__ == '__main__':
+
+	r = tkinter.Tk()
+	e = Editor(r)
+	e.mainloop()
+
 		
