87877c1b53d8551c7ea039996d27235b84fa6303
SamuelKos <89200126+SamuelKos@users.noreply.github.com>
2024-06-04 12:51:16 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -10,41 +10,29 @@ class TestGui(unittest.TestCase):
 		self.app.mainloop()
 	
 	
-##	async def _doit(self):
-##		self.app.wait_variable(self.app.waitvar)
+	async def _doit(self):
+		self.app.wait_variable(self.app.waitvar)
 		
 	
 	def setUp(self):
 		self.app = start_application()
 		self._start_app()
+		self._doit()
+
 	
 	def tearDown(self):
 		self.app.destroy()
+
 	
 	def test_startup(self):
 ##		title = self.app.winfo_toplevel().title()
 ##		expected = 'Titteli'
 ##		self.assertEqual(title, expected)
-		value = 'no'
+		#value = 'no'
 		expected = 3
-		i = 0
-		
-		while value == 'no':
-			self.app.update_idletasks()
-			value = self.app.waitvar.get()
 			
-			if not value:
-				value = 'no'
-				
-			time.sleep(1)
-			i += 1
-			
-			if i > 3:
-				break
-			
-			#value = self.app.wait_variable(self.app.waitvar)
-			
-		print(i, 'AAAAAAAA')
+		value = self.app.waitvar.get()
+		#print(i, 'AAAAAAAA')
 		self.assertEqual(value, expected)
 		
 
